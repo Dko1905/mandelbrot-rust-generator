a5ccda4078d7427b36a7cd1577e6ab70f4aa3bd1
Daniel Florescu <daniel.florescu1905@protonmail.ch>
2020-07-08 16:34:45 +0200

    Now works with color

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 // For reading and opening files
-use std::path::Path;
 use std::fs::File;
 use std::io::BufWriter;
 use std::io::Write;
@@ -7,24 +6,42 @@ use std::io::Write;
 use num::Complex;
 
 fn main() {
-	let path = Path::new(r"./image.png");
-	
-	generator(path, 400, 400).expect("Failed to generate png file");
+	let size: u32 = 1200;
+	let max = 10;
+	let mut scale: f64 = 100.;
+
+	let mut thread_handles: Vec<std::thread::JoinHandle<()>> = Vec::new(); 
+
+	for n in 0..max{
+		let handle = std::thread::spawn(move || {
+			generator(&format!("image{}.png", n), size, size, scale).expect("Failed to generate images");
+		});
+		thread_handles.push(handle);
+		
+		scale = scale * 2.;
+		println!("n = {}", n);
+	}
+
+	let mut n = 0;
+	for handle in thread_handles {
+		handle.join().expect("Failed to join handle");
+		println!("{}", n);
+		n += 1;
+	}
 }
 
-fn generator(output_path: &Path, width: u32, height: u32) -> Result<(), std::boxed::Box<dyn std::error::Error>> {
+fn generator(output_path: &str, width: u32, height: u32, scale: f64) -> Result<(), std::boxed::Box<dyn std::error::Error>> {
 	let file: File = File::create(output_path)?;
 	let mut png_encoder = png::Encoder::new(BufWriter::new(file), width, height);
-	png_encoder.set_color(png::ColorType::Grayscale);
+	png_encoder.set_color(png::ColorType::RGB);
 	png_encoder.set_depth(png::BitDepth::Eight);
 
 	let mut png_writer = png_encoder.write_header()?;
 	let mut swriter = png_writer.stream_writer();
 
-	let seed = 1.;
+	let seed: f64 = 1.;
 	let precision = 100.;
-	let scale = 200.;
-	let x_offset = 0;
+	let x_offset = 2./3. - (486325./10000000.);
 	let y_offset = 0;
 
 	for num_y in 0..(height) {
@@ -38,7 +55,7 @@ fn generator(output_path: &Path, width: u32, height: u32) -> Result<(), std::box
 
 			let mut z = Complex::new(x, y).powf(2.) - seed;
 			
-			let color: u8;
+			
 			while count < precision as u64 {
 				z = z.powf(2.) - seed;
 
@@ -50,13 +67,24 @@ fn generator(output_path: &Path, width: u32, height: u32) -> Result<(), std::box
 				count += 1;
 			}
 
+			let mut color: [u8; 3] = [0; 3];
 			if z.norm() <= 0. {
-				color = 0;
+				color[0] = 0;
+				color[1] = 0;
+				color[2] = 0;
 			}
 			else {
-				color = (255 - check % 255) as u8;
+				let mut hsl_color = colorsys::Hsl::default();
+				hsl_color.set_hue((check * 10 % 255) as f64);
+				hsl_color.set_saturation(100.);
+				hsl_color.set_lightness(50.);
+				let a = colorsys::Rgb::from( hsl_color );
+				color[0] = a.get_red() as u8;
+				color[1] = a.get_green() as u8;
+				color[2] = a.get_blue() as u8;
 			}
-			swriter.write(&[color]).expect("Failed to write");
+			
+			swriter.write(&color).expect("Failed to write");
 		}
 	}
 
